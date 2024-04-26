a3dfdfb4b9ad39841216c2515a959bf853f018a8
regislaine <regislaine.regis@ab-inbev.com>
2024-04-26 19:23:44 -0300

    refactor: refatoracao

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      String nome = "Regislaine";
+      int numero = 20;
+      final double PI = 3.14;
     }
 }
