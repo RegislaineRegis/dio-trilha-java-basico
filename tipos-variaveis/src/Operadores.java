8ce351d8a750eca3fcd3087f0d057ad52f1c486b
regislaine <regislaine.regis@ab-inbev.com>
2024-04-26 18:05:39 -0300

    feat: aula inicial operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = " LINGUAGEM" + "JAVA";
+        String nomeSobrenome = " REGISLAINE" + " " + "REGIS" ;
+
+        System.out.println("Nome Completo:" + nomeCompleto);
+        System.out.println("Nome Sobrenome:" + nomeSobrenome);
+    }
+}
